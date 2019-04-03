6fc3d1fe0d76c5e9dd32433f9a9f62431acf1af2
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2019-04-03 20:43:22 +0300

    Initial prototype of Duration and Clocks API

---
+++ libraries/stdlib/jvm/src/kotlin/time/MonoClock.kt
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2010-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+package kotlin.time
+
+public actual object MonoClock : LongReadingClock(), Clock { // TODO: interface should not be required here
+    override fun reading(): Long = System.nanoTime()
+    override val unit: DurationUnit = DurationUnit.NANOSECONDS
+}
