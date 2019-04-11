e63d0ab56eda127ec2439dc68068bfec5f8daeaa
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2019-04-11 19:59:47 +0300

    Implement MonoClock in JS
    It uses either process.hrtime() in node.js or performance.now() in browser,
    falling back to Date.now() if the latter is not available

--- libraries/stdlib/js/src/kotlin/time/MonoClock.kt
+++ libraries/stdlib/js/src/kotlin/time/MonoClock.kt
@@ -5,8 +5,49 @@
 
 package kotlin.time
 
-public actual object MonoClock : Clock {
-    override fun mark(initialElapsed: Duration): ClockMark {
-        TODO("not implemented: base on high-res browser or node perf counter")
+import org.w3c.performance.GlobalPerformance
+import org.w3c.performance.Performance
+
+actual object MonoClock : Clock {
+
+    private val actualClock: Clock = run {
+        val isNode: Boolean = js("typeof process !== 'undefined' && process.versions && !!process.versions.node")
+
+        if (isNode)
+            HrTimeClock(js("process").unsafeCast<Process>())
+        else
+            js("self").unsafeCast<GlobalPerformance?>()?.performance?.let(::PerformanceClock)
+                ?: DateNowClock
+
     }
+
+    override fun mark(initialElapsed: Duration): ClockMark = actualClock.mark(initialElapsed)
+}
+
+internal external interface Process {
+    fun hrtime(time: Array<Double> = definedExternally): Array<Double>
+}
+
+internal class HrTimeClock(val process: Process) : Clock {
+
+    override fun mark(initialElapsed: Duration): ClockMark = object : ClockMark {
+        val startedAt = process.hrtime()
+        override val clock: Clock get() = this@HrTimeClock // delegation problem?
+        override val elapsedFrom: Duration
+            get() = process.hrtime(startedAt).let { (seconds, nanos) -> seconds.seconds + nanos.nanoseconds + initialElapsed }
+    }
+
+    override fun toString(): String = "process.hrtime()"
+}
+
+internal class PerformanceClock(val performance: Performance) : DoubleReadingClock() {
+    override val unit: DurationUnit get() = DurationUnit.MILLISECONDS
+    override fun reading(): Double = performance.now()
+    override fun toString(): String = "self.performance.now()"
+}
+
+internal object DateNowClock : DoubleReadingClock() {
+    override val unit: DurationUnit get() = DurationUnit.MILLISECONDS
+    override fun reading(): Double = kotlin.js.Date.now()
+    override fun toString(): String = "Date.now()"
 }
